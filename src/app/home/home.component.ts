fc33687b609646fa3df44b54dc6bd075d6730fd3
Benjamin Wyatt <bpwyatt04@gmail.com>
2024-10-01 21:42:22 -0700

    Show more images on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { MatButtonModule } from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
@@ -7,14 +7,21 @@ import { Router } from '@angular/router';
 import { ImageComponent } from '../image/image.component';
 import { GalleryComponent } from '../gallery/gallery.component';
 import { DomSanitizer } from '@angular/platform-browser';
+import { CollectionsService } from '../collections/collections.service';
+import { Image } from '../gallery/gallery.service';
+import { getRandomImage } from '../utilities/imageUtils';
+import { NgClass } from '@angular/common';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    NgClass,
     NavComponent,
     MatGridListModule,
     MatButtonModule,
+    MatCard,
     MatIconModule,
     ImageComponent,
     GalleryComponent
@@ -22,11 +29,43 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  images: Image[] = [];
+  image!: Image;
+  interval!: NodeJS.Timeout;
+  shouldTransition: boolean = false;
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private router: Router) {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private changeDetectorRef: ChangeDetectorRef, private collectionsService: CollectionsService, private router: Router, private zone: NgZone) {
     iconRegistry.addSvgIcon('instagram', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/instagram.svg'));
     iconRegistry.addSvgIcon('threads', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/threads.svg'));
+  }
+
+  ngOnInit(): void {
+    const shuffle = (array: Image[]) => { 
+      for (let i = array.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]]; 
+      } 
+      return array; 
+    }; 
+
+    this.images = shuffle(this.collectionsService.getAllImages());
+    this.image = getRandomImage(this.images);
+    // this.zone.runOutsideAngular(() => {
+    //   this.interval = setInterval(() => {
+    //     setTimeout(() => {
+    //       this.shouldTransition = true;
+    //       this.changeDetectorRef.detectChanges();
+    //     }, 9000);
+    //     this.shouldTransition = false;
+    //     this.image = getRandomImage(this.images);
+    //     this.changeDetectorRef.detectChanges();
+    //   }, 10000);
+    // });
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 
   onClickBlogFab() {
